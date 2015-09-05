72180fa12e7a22b1fd902406fc29a064b7b28574
Emanuele Forestieri <forestieriemanuele@gmail.com>
2015-09-05 17:28:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -14,17 +14,17 @@ bool controlla(unsigned char *s,unsigned char *c,unsigned short start,unsigned s
 {
 	bool t=1;
 	for(unsigned short i=start;i!=end;i++)
-    if(verifica(s,*(c+i))>q)
+     	if(verifica(s,*(c+i))>q)
 		{
-			 t=0;
-			 break;
-	  }
+			t=0;
+			break;
+		}
 	return t;
 }
 void genera(unsigned char *s,unsigned char *c)
 {
 	for(unsigned short i=0;i!=10;i++)
-    *(s+i)=*(c+rand()%+16);
+     	*(s+i)=*(c+rand()%+16);
 }
 void main()
 {
@@ -32,8 +32,8 @@ void main()
 	srand(time(NULL));
 	while(1)
 	{
-    genera(s,c);
+     	genera(s,c);
 		if(controlla(s,c,0,10,2)&&controlla(s,c,10,16,3))
-      printf("%s\n",s);	
+      		printf("%s\n",s);	
 	}
 }
