4572f865e3a82b13719671f4c8927f77764875f8
Emanuele Forestieri <forestieriemanuele@gmail.com>
2015-09-11 10:51:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -51,10 +51,10 @@ unsigned short cinque(unsigned char *s,unsigned char *c)
 }
 void genera(unsigned char *s,unsigned char *c)
 {
-    for(unsigned short i=0;i!=10;i++)
-        *(s+i)=*(c+rand()%+16);
+    while(*s)
+        *(s++)=*(c+rand()%+16);
 }
-void main()
+int main()
 {
     unsigned char s[11]="XXXXXXXXXX\0",c[17]="0123456789ABCDEF\0";
     srand(time(NULL));
@@ -64,4 +64,5 @@ void main()
         if(controlla(s,c,0,10,2)&&controlla(s,c,10,16,3)&&tripli(s,c)&&cinque(s,c)<5)
             printf("%s\n",s);   
     }
+    return EXIT_SUCCESS;
 }
